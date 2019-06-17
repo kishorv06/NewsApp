b52ebfc06275e03da6304b6948e0f85cde41a1d6
Kishor V <developer.kishor@gmail.com>
2019-06-17 16:02:38 +0530

    Added News Details Activity

---
+++ app/src/main/java/in/microid/krv/newsapiapp/News.java
@@ -0,0 +1,13 @@
+package in.microid.krv.newsapiapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class News extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
