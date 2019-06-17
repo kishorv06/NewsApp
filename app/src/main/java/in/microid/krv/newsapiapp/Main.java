702085c21800cfd5d93678ba4c8c22c766db339b
Kishor V <developer.kishor@gmail.com>
2019-06-17 10:59:09 +0530

    Removed Actionbar from the theme.

--- app/src/main/java/in/microid/krv/newsapiapp/Main.java
+++ app/src/main/java/in/microid/krv/newsapiapp/Main.java
@@ -1,7 +1,10 @@
 package in.microid.krv.newsapiapp;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
