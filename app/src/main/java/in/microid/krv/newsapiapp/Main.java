03c30428882eb6d5b05be0cb953a7ced9f2ca547
Kishor V <developer.kishor@gmail.com>
2019-06-17 15:50:52 +0530

    Added Main activity

--- app/src/main/java/in/microid/krv/newsapiapp/Main.java
+++ app/src/main/java/in/microid/krv/newsapiapp/Main.java
@@ -1,12 +1,25 @@
 package in.microid.krv.newsapiapp;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Main extends AppCompatActivity {
+    private API api;
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +29,65 @@ public class Main extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+        rv = findViewById(R.id.feeds_recycler);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        api = new API(this);
+        if (sp.getString("news", null) == null) {
+            refreshNews();
+        } else {
+            try {
+                JSONArray news = new JSONArray(sp.getString("news", "{}"));
+                loadNEWS(news);
+            } catch (JSONException e) {
+                onError(e);
+            }
+        }
+        SwipeRefreshLayout swp = findViewById(R.id.feeds_swipe);
+        swp.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshNews();
+            }
+        });
+    }
+
+    private void refreshNews() {
+        final SwipeRefreshLayout swp = findViewById(R.id.feeds_swipe);
+        swp.setRefreshing(true);
+        api.getNEWS(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getString("status").equals("ok")) {
+                        loadNEWS(response.getJSONArray("articles"));
+                        PreferenceManager.getDefaultSharedPreferences(Main.this)
+                                .edit()
+                                .putString("news", response.getJSONArray("articles").toString())
+                                .apply();
+                        swp.setRefreshing(false);
+                    }
+                } catch (Exception e) {
+                    onError(e);
+                    swp.setRefreshing(false);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                onError(error);
+                swp.setRefreshing(false);
+            }
+        });
+    }
+
+    private void loadNEWS(JSONArray news) {
+        NewsRecyclerViewAdapter adapter = new NewsRecyclerViewAdapter(news);
+        rv.setAdapter(adapter);
+    }
+
+    private void onError(Exception e) {
+        e.printStackTrace();
+        Toast.makeText(this, "Failed to load news feeds.", Toast.LENGTH_SHORT).show();
     }
 }
