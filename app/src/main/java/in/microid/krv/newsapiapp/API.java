3a837539a069b67ec328ea53930736c194c9a0c2
Kishor V <developer.kishor@gmail.com>
2019-06-17 11:33:56 +0530

    Created an API for making request to newsapi.org

---
+++ app/src/main/java/in/microid/krv/newsapiapp/API.java
@@ -0,0 +1,31 @@
+package in.microid.krv.newsapiapp;
+
+import android.content.Context;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
+public class API {
+    private RequestQueue queue = null;
+    private Context context = null;
+
+    public API(Context c) {
+        context = c;
+        queue = Volley.newRequestQueue(c);
+    }
+
+    public void getNEWS(Response.Listener<JSONObject> success, Response.ErrorListener error) {
+        sendRequest("country=in", success, error);
+    }
+
+    public void sendRequest(String query, Response.Listener<JSONObject> success, Response.ErrorListener error) {
+        String url = context.getResources().getString(R.string.api_url)
+                + "apiKey=" + context.getResources().getString(R.string.api_token)
+                + "&" + query;
+        JsonRequest request = new JsonObjectRequest(url, null, success, error);
+        queue.add(request);
+    }
+}
